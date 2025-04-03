2a188c4ae649791ff6b26d50bc233a3e6892423b
ppires21 <pepepires@hotmail.com>
2025-04-03 20:19:01 +0100

    Serviço de autenticação sem erros

---
+++ Servico-Auth/src/main/java/com/serviceauth/servicoauthentic/ServicoAuthenticApplication.java
@@ -0,0 +1,13 @@
+package com.serviceauth.servicoauthentic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoAuthenticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoAuthenticApplication.class, args);
+    }
+
+}
---
+++ Servico-Auth/src/test/java/com/serviceauth/servicoauthentic/ServicoAuthenticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceauth.servicoauthentic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoAuthenticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
