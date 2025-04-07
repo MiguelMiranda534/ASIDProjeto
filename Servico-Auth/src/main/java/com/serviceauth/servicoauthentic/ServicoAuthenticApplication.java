7cb63a30c3a49ea2facc9c951238a3d16eb260e9
ppires21 <pepepires@hotmail.com>
2025-04-07 18:40:51 +0100

    Eureka não funciona direito

--- Servico-Auth/src/main/java/com/serviceauth/servicoauthentic/ServicoAuthenticApplication.java
+++ Servico-Auth/src/main/java/com/serviceauth/servicoauthentic/ServicoAuthenticApplication.java
@@ -1,9 +1,9 @@
 package com.serviceauth.servicoauthentic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServicoAuthenticApplication {
 
     public static void main(String[] args) {
