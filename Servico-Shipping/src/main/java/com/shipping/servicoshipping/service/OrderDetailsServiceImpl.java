24fb3c25a492fa85a901022ac7566cca095a0705
ppires21 <pepepires@hotmail.com>
2025-04-08 00:42:01 +0100

    Eureka adicionado (mas ainda não totalmente configurado) API Gateway configurado com eureka e os 2 microserviços que o pires dividiu

--- Servico-Shipping/src/main/java/com/shipping/servicoshipping/service/OrderDetailsServiceImpl.java
+++ Servico-Shipping/src/main/java/com/shipping/servicoshipping/service/OrderDetailsServiceImpl.java
@@ -20,8 +20,4 @@ public class OrderDetailsServiceImpl implements OrderDetailsService {
 
     }
 
-    @Override
-    public List<OrderDetails> getAllOrders() {
-        return orderDetailsRepository.findAll();
-    }
 }
