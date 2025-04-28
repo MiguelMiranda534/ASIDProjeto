1e864e98ff8dc5e5a1c7698baac7e20e6b260d79
ppires21 <pepepires@hotmail.com>
2025-04-28 05:55:06 +0100

    Shipping: Criar shippingorder faz criar order Adicionar orderdetails à shippingorder faz atualizar order
    CQRS Funcional

---
+++ Servico-Query/src/main/java/com/projeto/servicoquery/ServicoQueryApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.servicoquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoQueryApplication.class, args);
+	}
+
+}
---
+++ Servico-Query/src/test/java/com/projeto/servicoquery/ServicoQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.servicoquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
